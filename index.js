dc9eac0d1a465e465e68e0a1ad190cf6d713ca47
Stan Kudrow <kudrow.stan@gmail.com>
2016-02-10 12:08:04 -0500

    assert to ok

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var assert = require('assert');
+var ok = require('assert');
 var pg = require('pg');
 
 module.exports = function init (config) {
-  assert(config.user, 'user required');
-  assert(config.password, 'password required');
-  assert(config.host, 'host required');
-  assert(config.db, 'db required');
+  ok(config.user, 'user required');
+  ok(config.password, 'password required');
+  ok(config.host, 'host required');
+  ok(config.db, 'db required');
 
   var conString = 'postgres://' + config.user + ':' + config.password + '@' + config.host +
   ':' + (config.port || 5439) + '/' + config.db;
